cc053aeb2bae9b9cd37f6b377f8e95799995d2d7
James <N3rdP1um23@users.noreply.github.com>
2020-01-10 22:44:46 -0500

    Added file to repo that handles the backup status
    1. If no stream is detected and no backup currently running, start the backup
    2. If a stream it detected, start the backup

---
+++ PlexBackup.sh
@@ -0,0 +1,25 @@
+###
+#
+# N3rdP1um23
+# January 10, 2020
+# v1.0
+# This file is used to run in the background to perform the backup of your server.
+# 1. If a zero (0) Plex streams are detected & a backup is not currently running, the back will start
+# 2. If a Plex stream is detected, the backup background task is killed
+#
+###
+
+#!/bin/bash
+
+## Get the number of streams for Plex & chack if there is a running backup
+STREAM_COUNT=`/usr/bin/custom/CurrentPlexStreams.sh`
+BACKUP_STATUS=`ps -ef | grep "rclone --transfer*" | grep -v grep | head -1 | awk '{print $2}'`
+
+## Check if there are no plex streams & no current backup
+if [ $STREAM_COUNT -eq 0 ] && [ -z "$BACKUP_STATUS" ]; then
+	## Perform the Backup
+	rclone --transfers=4 --checkers=35 --drive-chunk-size=256M --drive-upload-cutoff=256M --bwlimit=52M --progress -v  sync <TO> <FROM>
+elif [ $STREAM_COUNT -gt 0 ]; then
+	## Kill transfer if there is a stream
+	kill $BACKUP_STATUS
+fi
